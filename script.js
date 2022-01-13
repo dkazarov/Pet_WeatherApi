2abea74f1427868406319b9eb952bd3a4764688c
Dima Kazarov <rockat86@gmail.com>
2022-01-13 18:27:11 +0200

    Conect Api and create Base functional

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 'use strict';
+
+const param = {
+	"url" : "https://api.openweathermap.org/data/2.5/",
+	"appid" : "df6323c09e8d7869991460f3e64324f5"
+}
+
+const cities = {
+   703448 : "Київ",
+   706483 : "Харків",
+   698740 : "Одесса",
+   709930 : "Дніпро",
+   709717 : "Донецьк",
+   687700 :"Запоріжжя",
+         : "Львів",
+         : "Кривий Ріг",
+         : "Миколаїв",
+         : "Севастополь",
+         : "Маріуполь",
+         : "Луганськ",
+         : "Вінниця",
+         : "Сімферополь",
+         : "Макіївка",
+         : "Херсон",
+         : "Чернігів",
+         : "Полтава",
+         : "Хмельницький",
+         : "Черкаси";
+         : "Чернівці";
+         : "Житомир",
+         : "Суми",
+         : "Рівне",
+         : "Горлівка",
+         : "Івано-Франківськ",
+         : "Кам'янське",
+         : "Кропивницький",
+         : "Тернопіль",
+         : "Луцьк"
+}
+
+function getWeather() {
+	const cityId = document.querySelector('#city').value;
+	fetch(`${param.url}weather?id=${cityId}&units=metric&APPID=${param.appid}`)
+	.then(weather => { 
+			return weather.json();
+		}).then(showWeather);
+   console.log(cityId);
+}
+
+function showWeather(data) {
+	console.log(data);
+}
+
+
+
+
+
+getWeather();
+
+document.querySelector('#city').onchange = getWeather;
